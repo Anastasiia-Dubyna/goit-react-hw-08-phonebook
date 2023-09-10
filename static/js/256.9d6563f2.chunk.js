12dc54ced262fedba99f85459d889393a9da86f3
Anastasiia-Dubyna <122117575+Anastasiia-Dubyna@users.noreply.github.com>
2023-09-10 14:35:31 +0000

    Deploying to gh-pages from @ Anastasiia-Dubyna/goit-react-hw-08-phonebook@c4eba42d7129118b6aae0c4a54c577d00b3393ef 🚀

---
+++ static/js/256.9d6563f2.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[256],{9256:function(e,r,t){t.r(r);t(2791);var a=t(1087),o=t(184);r.default=function(){return(0,o.jsxs)("div",{children:["Error 404. Sorry, page wasn`t found.",(0,o.jsx)(a.rU,{to:"/",children:"Back to the Home page."})]})}}}]);
+//# sourceMappingURL=256.9d6563f2.chunk.js.map
